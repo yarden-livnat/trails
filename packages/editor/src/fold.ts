6783333db335be3816583540e1b182c150e1fa81
Yarden Livnat <yarden.livnat@gmail.com>
2017-11-08 15:19:04 -0700

    work in progress

--- packages/editor/src/fold.ts
+++ packages/editor/src/fold.ts
@@ -4,17 +4,43 @@ import 'codemirror/addon/fold/foldcode';
 import 'codemirror/addon/fold/foldgutter';
 import 'codemirror/addon/fold/comment-fold';
 
+function isBlock(cm, line) {
+  console.log('isBlock:', line, cm.getLineTokens(line));
+  return cm.getTokenTypeAt(CodeMirror.Pos(line, 0)) == 'line-annotation';
+  // let tokens = cm.getLineTokens(line);
+  // console.log(`isBlock [${line}]`, tokens);
+  // return tokens.length > 1 && tokens[0] == 'soa' && tokens[1] == 'block';
+}
+
 CodeMirror.registerHelper('fold', 'trails', (cm: any, start: any) => {
   let line = start.line;
-  let text = cm.getLine(line);
-  if (!/^-- @/.test(text)) return;
+
+  if (!isBlock(cm, line)) return;
 
   let from = CodeMirror.Pos(line, cm.getLine(line).length);
   let end = cm.lastLine();
-  while (++line <= end && !/^-- @/.test(cm.getLine(line)));
+  while (++line <= end && !isBlock(cm, line));
 
   return {
     from: from,
     to: CodeMirror.Pos(line-1,cm.getLine(line-1).length)
   }
+
+  // let tokens = cm.getLineTokens(start.line);
+  // console.log('tokens', tokens);
+  // if (tokens.length > 1 && tokens[0] == 'pre' && tokens[1] == 'block') {
+  //   let end = cm
+  // }
+  //
+  // let text = cm.getLine(line);
+  // if (!/^-- @/.test(text)) return;
+  //
+  // let from = CodeMirror.Pos(line, cm.getLine(line).length);
+  // let end = cm.lastLine();
+  // while (++line <= end && !/^-- @/.test(cm.getLine(line)));
+  //
+  // return {
+  //   from: from,
+  //   to: CodeMirror.Pos(line-1,cm.getLine(line-1).length)
+  // }
 });
--- packages/editor/src/mode.ts
+++ packages/editor/src/mode.ts
@@ -1,22 +1,23 @@
 import * as CodeMirror from 'codemirror';
 import 'codemirror/mode/sql/sql';
 
-const pre = {style: 'pre-block', pattern: /--\s+(?=@)/};
-const post = {style: 'post-block', pattern: /$/};
+const pre = {token: 'pre', pattern: /^\s*--\s+(?=@)/};
+const post = {token: 'eol', pattern: /$/};
 
 let annotations = [
-  {style: 'annotation block', pattern: /^@block(?=\s|$)/},
-  {style: 'annotation', pattern: /@into(?=\s|$)/},
-  {style: 'annotation', pattern: /^@report(?=s\|$)/},
-  {style: 'annotation', pattern: /^@info(?=\s|$)/},
-  {style: 'annotation', pattern: /^@debug(?=s\|$)/},
-  {style: 'annotation', pattern: /^@use(?=\s|$)/},
-  {style: 'annotation', pattern: /^@proc|@procedure|@function(?= |$)/},
+  {token: 'block', pattern: /^@block(?=\s|$)/i},
+  {token: 'into', pattern: /i@into(?=\s|$)/i},
+  {token: 'report', pattern: /^@report(?=s\|$)/i},
+  {token: 'info', pattern: /^@info(?=\s|$)/i},
+  {token: 'debug', pattern: /^@debug(?=s\|$)/i},
+  {token: 'use', pattern: /^@use(?=\s|$)/i},
+  {token: 'proc', pattern: /^@proc|@procedure|@function(?= |$)/i},
+  {token: 'incomplete', pattern: /^@\w*/}
 ];
 
 let keywords = [
-  {style: 'keyword', pattern: /^GO/i},
-  {style: 'eos', pattern: /^;/}
+  {token: 'keyword', pattern: /^GO/i},
+  {token: 'eos', pattern: /^;/}
 ];
 
 let keys = Object.keys(annotations);
@@ -27,49 +28,79 @@ CodeMirror.defineMode("trails-sql", function(config :any, parserConfig) {
     name: mode
   });
 
-  function trails(stream: any, state: any) {
-    // console.log('peek:', stream.pos, stream.peek());
+  function token(stream, state) {
+    if (stream.eatSpace()) return null;
+
+    for (let annotation of annotations) {
+      if (stream.match(annotation.pattern))
+        return 'annotation';
+    }
+
+    if (stream.match(/\S+/)) return 'word';
+
+    stream.skipToEnd();
+    return 'desc';
+  }
+
+  function parser_trails(stream, state ) {
+    let cx = state.context;
+    let prev = cx.token;
+
+    if (prev == 'word') {
+      stream.skipToEnd();
+      return 'desc';
+    }
+
+    let style = token(stream, state);
+    if (!style) return;
+
+    if (style == 'eol') {
+      state.pop();
+      return style;
+    }
+
+    if (prev == 'soa') {
+      if (style != 'annotation') return error(style, stream, state);
+      cx.token = style;
+      return style;
+    }
+
+    if (prev == 'annotation') {
+      if (style == 'word') {
+        cx.token = style;
+        return style;
+      }
+      stream.skipToEnd();
+      return 'desc';
+    }
+
+    console.log("bug: shouldn't get here");
+  }
+
+  function parser(stream: any, state: any) {
     if (state.context && state.context.trails) {
-      if (stream.eatSpace()) return null;
-      let type = state.context.type;
-      let m;
-
-      if (type == 'pre') {
-        for (let annotation of annotations) {
-          if (stream.match(annotation.pattern)) {
-            state.type = 'annotation';
-            return annotation.style;
-          }
-        }
-        if (stream.eatWhile(/\w/)) {
-          state.type = 'annotation';
-          return 'word';
-        }
-        
-      }
-      if (type == 'annotation') {
-        if ((m = stream.match(/\w+/))) {
-          state.type = 'name';
-          return 'name';
-        }
-      }
-      if (type == 'name') {
-        stream.skipToEnd();
-        return 'desc';
-      }
-
-      if (stream.match(post.pattern)) {
-        console.log('eol');
+      let style = parser_trails(stream, state);
+      if (stream.eol()) { console.log('eol');
         pop(state);
-        return post.style;
       }
-    }
-    if (stream.match(pre.pattern)) {
-      push(state);
-      return pre.style;
+      return style;
     }
 
-    return null;
+    if (stream.sol()) {
+      if (stream.match(pre.pattern)) {
+        push(state);
+        return "line-annotation";
+      }
+    }
+
+    return sql.token(stream, state);
+  }
+
+  function error(style, stream, state) {
+    state.error = true;
+    stream.skipToEnd();
+    state.context.token = 'error';
+    return style + ' error'
   }
 
   function push(state) {
@@ -78,7 +109,7 @@ CodeMirror.defineMode("trails-sql", function(config :any, parserConfig) {
       // indent: stream.indentation(),
       // col: stream.column(),
       trails: true,
-      type: 'pre'
+      token: 'soa'
     };
   }
 
@@ -91,7 +122,7 @@ CodeMirror.defineMode("trails-sql", function(config :any, parserConfig) {
       return sql.startState();
     },
     token: function(stream, state) {
-      return trails(stream, state) || sql.token(stream, state);
+      return parser(stream, state);
     },
     indent: sql.indent,
     blockCommentStart: sql.blockCommentStart,
