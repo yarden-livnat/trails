cf7115f543b2e0782b7e86ad4bc07e29ccb6ec7c
Yarden Livnat <yarden.livnat@gmail.com>
2017-12-12 10:17:28 -0700

    init trails package

---
+++ packages/trails/src/trails.ts
@@ -0,0 +1,20 @@
+
+import {
+  Token
+} from '@phosphor/coreutils';
+
+export
+const ITrails = new Token<ITrails>('@trails/trails');
+
+
+export
+interface ITrails {
+
+}
+
+
+
+export
+class Trails implements ITrails {
+
+}
